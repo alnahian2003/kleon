043eeed54737f0571b403f2d4ec012b7db2a15e6
alnahian2003 <a.alnahian2003@gmail.com>
2023-04-01 23:37:38 +0600

    Email Verification Enabled

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,49 +1,69 @@
 <template>
-  <GuestLayout>
-    <Head title="Email Verification"/>
+    <GuestLayout>
+        <Head title="Email Verification" />
 
-    <div class="flex flex-col overflow-y-auto md:flex-row">
-      <div class="h-32 md:h-auto md:w-1/2">
-        <img aria-hidden="true" class="object-cover w-full h-full"
-             src="{{ asset('images/forgot-password-office.jpeg') }}" alt="Office"/>
-      </div>
-      <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
-        <div class="w-full">
-          <h1 class="mb-4 font-semibold text-gray-700">
-            Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we
-            just emailed to you? If you didn't receive the email, we will gladly send you another.
-          </h1>
+        <div class="flex flex-col overflow-y-auto md:flex-row">
+            <div class="h-32 md:h-auto md:w-1/2">
+                <img
+                    aria-hidden="true"
+                    class="object-cover w-full h-full"
+                    src="images/forgot-password-office.jpeg"
+                    alt="Office"
+                />
+            </div>
+            <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+                <div class="w-full">
+                    <h1 class="mb-4 font-medium text-gray-700">
+                        Thanks for signing up! Before getting started, could you
+                        verify your email address by clicking on the link we
+                        just emailed to you? If you didn't receive the email, we
+                        will gladly send you another.
+                    </h1>
 
-          <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent">
-            A new verification link has been sent to the email address you provided during registration.
-          </div>
+                    <div
+                        class="mb-4 font-medium text-sm text-green-600"
+                        v-if="verificationLinkSent"
+                    >
+                        A new verification link has been sent to the email
+                        address you provided during registration.
+                    </div>
 
-          <form @submit.prevent="submit">
-            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-              Resend Verification Email
-            </PrimaryButton>
+                    <form @submit.prevent="submit" class="space-y-4">
+                        <PrimaryButton
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        >
+                            Resend Verification Email
+                        </PrimaryButton>
 
-            <Link :href="route('logout')" method="post" as="button" class="underline text-sm text-gray-600 hover:text-gray-900">
-              Log Out
-            </Link>
-          </form>
+                        <Link
+                            :href="route('logout')"
+                            method="post"
+                            as="button"
+                            class="block underline text-sm text-gray-600 hover:text-gray-900"
+                        >
+                            Log Out
+                        </Link>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </GuestLayout>
+    </GuestLayout>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { computed } from "vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 const props = defineProps({
     status: String,
 });
-const form = useForm();
+const form = useForm({});
 const submit = () => {
-    form.post(route('verification.send'));
+    form.post(route("verification.send"));
 };
-const verificationLinkSent = computed(() => props.status === 'verification-link-sent');
+const verificationLinkSent = computed(
+    () => props.status === "verification-link-sent"
+);
 </script>
