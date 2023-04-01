f087ec86ad1b8340f57519ebaf356c9ced458586
alnahian2003 <a.alnahian2003@gmail.com>
2023-04-01 19:26:38 +0600

    Font Updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
+
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
